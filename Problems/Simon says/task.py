4a286d23222c075a836612863d136ebb496766e2
Cirt <Miro.Hud@gmail.com>
2020-06-12 20:01:58 +0200

    remove everything

--- Problems/Simon says/task.py
+++
@@ -1,7 +0,0 @@
-def what_to_do(instructions):
-    i = instructions.find('Simon says')
-    if instructions.startswith('Simon says'):
-        return f'I {instructions[i + 11:]}'
-    if instructions.endswith('Simon says'):
-        return f'I {instructions[:i - 1]}'
-    return 'I won\'t do it!'
